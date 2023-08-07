c952b0a0138b26ff3fc73280f4225b034374a7da
Ertan Kara <ertan.kara@movember.com>
2023-08-07 10:04:43 +0300

    Use component instead of duplicate <hr>

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Divider } from "@/app/components/Divider";
 import { GoogleLogInBtn } from "../components/GoogleLoginBtn";
 
 export default function Login() {
@@ -7,7 +8,7 @@ export default function Login() {
     <>
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Hello!</h1>
       <p className="mb-3 text-center text-gray-500">{"Great to meet you! Please continue with one of the following options to get to know each other!"}</p>
-      <hr className="w-64 h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700" />
+      <Divider />
       <GoogleLogInBtn />
     </>
   )
