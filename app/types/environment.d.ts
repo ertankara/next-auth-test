42b33bc38b7c1c22be947ec68547174895f69f9a
Ertan Kara <ertan.kara@movember.com>
2023-08-07 10:35:26 +0300

    Add secret for prod

--- app/types/environment.d.ts
+++ app/types/environment.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly GOOGLE_LOGIN_SECRET: string | undefined;
     readonly GOOGLE_LOGIN_ID: string | undefined;
+    readonly NEXTAUTH_SECRET: string | undefined;
   }
 }
