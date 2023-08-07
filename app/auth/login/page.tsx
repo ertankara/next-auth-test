aff23c18aeeb44a488ba2f7ec5576f02d877e5c7
Ertan Kara <ertan.kara@movember.com>
2023-08-07 10:17:27 +0300

    Use predefined component

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,11 +2,12 @@
 
 import { Divider } from "@/app/components/Divider";
 import { GoogleLogInBtn } from "../components/GoogleLoginBtn";
+import { Title } from "@/app/components";
 
 export default function Login() {
   return (
     <>
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Hello!</h1>
+      <Title text="Hello!" />
       <p className="mb-3 text-center text-gray-500">{"Great to meet you! Please continue with one of the following options to get to know each other!"}</p>
       <Divider />
       <GoogleLogInBtn />
