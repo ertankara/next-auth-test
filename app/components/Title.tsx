52397863cc302210cd3704a3e6e5aff2a3249884
Ertan Kara <ertan.kara@movember.com>
2023-08-07 10:17:16 +0300

    Capitalize text

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -4,6 +4,6 @@ type Props = {
 
 export const Title = (props: Props) => {
   return (
-    <h1 className="mb-4 text-4xl font-extrabold leading-none text-center tracking-tight text-gray-900 md:text-5xl lg:text-6xl">{props.text}</h1>
+    <h1 className="capitalize mb-4 text-4xl font-extrabold leading-none text-center tracking-tight text-gray-900 md:text-5xl lg:text-6xl">{props.text}</h1>
   )
 }
